ebba0ee25c5f09ab5321525486752148857f4886
aditya-shrimali <shrimaliaditya013@gmail.com>
2026-01-25 23:24:14 +0530

    seo metadata added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,8 +58,11 @@ export const metadata: Metadata = {
     canonical: "https://versusarenas.in",
   },
   icons: {
-    icon: '/icon.svg',
-    shortcut: '/icon.svg',
+    icon: [
+      { url: '/favicon.png', sizes: '512x512', type: 'image/png' },
+      { url: '/icon.svg', type: 'image/svg+xml' },
+    ],
+    shortcut: '/favicon.png',
   },
 };
 
@@ -94,7 +97,7 @@ const jsonLdGamesList = {
         '@type': 'VideoGame',
         name: 'Ludo Online',
         url: 'https://versusarenas.in/games/ludo',
-        image: 'https://versusarenas.in/games/ludo.png',
+        image: 'https://versusarenas.in/games/ludo3.png',
         description: 'Classic Ludo board game for 2-4 players',
       },
     },
@@ -104,8 +107,8 @@ const jsonLdGamesList = {
       item: {
         '@type': 'VideoGame',
         name: 'Business (Monopoly)',
-        url: 'https://versusarenas.in/games/business',
-        image: 'https://versusarenas.in/games/business.png',
+        url: 'https://versusarenas.in/games/monopoly',
+        image: 'https://versusarenas.in/games/business2.png',
         description: 'Property trading board game for 2-4 players',
       },
     },
@@ -118,6 +121,83 @@ const jsonLdGamesList = {
         url: 'https://versusarenas.in/games/snake-ladder',
         image: 'https://versusarenas.in/games/s&l.png',
         description: 'Classic dice game for 2-4 players',
+      },
+    },
+    {
+      '@type': 'ListItem',
+      position: 4,
+      item: {
+        '@type': 'VideoGame',
+        name: 'Poker Online',
+        url: 'https://versusarenas.in/games/poker',
+        image: 'https://versusarenas.in/games/poker1.png',
+        description: 'Texas Hold\'em Poker for 2-6 players',
+      },
+    },
+    {
+      '@type': 'ListItem',
+      position: 5,
+      item: {
+        '@type': 'VideoGame',
+        name: 'Chess Online',
+        url: 'https://versusarenas.in/games/chess',
+        image: 'https://versusarenas.in/games/chess2.png',
+        description: 'Classic Strategy game for 2 players',
+      },
+    },
+    {
+      '@type': 'ListItem',
+      position: 6,
+      item: {
+        '@type': 'VideoGame',
+        name: 'Tic Tac Toe',
+        url: 'https://versusarenas.in/games/tictactoe',
+        image: 'https://versusarenas.in/games/tictactoe.png',
+        description: 'Classic X and O game for 2 players',
+      },
+    },
+    {
+      '@type': 'ListItem',
+      position: 7,
+      item: {
+        '@type': 'VideoGame',
+        name: 'Sudoku',
+        url: 'https://versusarenas.in/games/sudoku',
+        image: 'https://versusarenas.in/games/sudoku2.png',
+        description: 'Number puzzle game',
+      },
+    },
+    {
+      '@type': 'ListItem',
+      position: 8,
+      item: {
+        '@type': 'VideoGame',
+        name: '2048',
+        url: 'https://versusarenas.in/games/2048',
+        image: 'https://versusarenas.in/games/2048-.png',
+        description: 'Slide tile puzzle game',
+      },
+    },
+    {
+      '@type': 'ListItem',
+      position: 9,
+      item: {
+        '@type': 'VideoGame',
+        name: 'Memory Flip',
+        url: 'https://versusarenas.in/games/memory',
+        image: 'https://versusarenas.in/games/memory1.png',
+        description: 'Card matching memory game',
+      },
+    },
+    {
+      '@type': 'ListItem',
+      position: 10,
+      item: {
+        '@type': 'VideoGame',
+        name: 'Candy Curse',
+        url: 'https://versusarenas.in/games/candy-curse',
+        image: 'https://versusarenas.in/games/candy1.png',
+        description: 'Match 3 puzzle game',
       },
     },
   ],
